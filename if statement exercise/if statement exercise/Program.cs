556db1a202948ac083a636154db0746b74656fd1
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-03-13 11:15:33 +1300

    task 5 if statement

---
+++ if statement exercise/if statement exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_statement_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a four digit pin number");
+            int pin1 = int.Parse(Console.ReadLine());
+
+            if (pin1 > 9999 && pin1 < 1000)
+            {
+
+                Console.WriteLine("please re-enter the pin");
+                int pin2 = int.Parse(Console.ReadLine());
+
+                if (pin1 == pin2)
+                    Console.WriteLine("pin accepted");
+
+                else
+                    Console.WriteLine("pin was not set");
+
+                Console.WriteLine("***************");
+                Console.ReadLine();
+
+            }
+
+
+
+        }
+    }
+}
---
+++ if statement exercise/if statement exercise/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("if statement exercise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("if statement exercise")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e72c13a9-3070-49b9-87b8-b9e70cb09278")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
