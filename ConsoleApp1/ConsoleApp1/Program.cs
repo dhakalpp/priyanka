21e01cd6dcb7eb851f6687200f78a7b5b3eb9e35
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-03-13 10:15:44 +1300

    task4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number i will tell you if it's odd or even");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+                Console.WriteLine("the num you gave me was even");
+            else
+                Console.WriteLine(" the num you gave me was odd");
+
+            Console.WriteLine("*************");
+            Console.ReadLine();
+        }  
+    }
+}
